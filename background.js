474b46fae21dfe18c81039503a2b611fa4d278e8
Oğuz Tanrıkulu <turbulence6th@gmail.com>
2021-05-23 18:08:17 +0300

    beni hatırla düzeltildi

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
-depo = null;
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.mesajTipi === "depoGetir") {
+    let depoStr = localStorage['depo'];
+    let depo = null;
+    if (depoStr) {
+      depo = JSON.parse(depoStr);
+    }
     sendResponse(depo);
   } else if (request.mesajTipi === "beniHatirla") {
-    depo = request.depo;
+    localStorage['depo'] = JSON.stringify(request.depo);
   }
 });
 chrome.runtime.onInstalled.addListener(function() {
