b3595791fb5ea84fdb9dea46fa84a96c6b4c6fca
Oğuz Tanrıkulu <turbulence6th@gmail.com>
2021-09-12 14:05:02 +0300

    npm projesi yapıldı

--- iframe/beniAc/beniAc.js
+++ iframe/beniAc/beniAc.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
+$(function() {
     efekUygula($('#ikon'), 3000);
 });
 
+/**
+ * 
+ * @param {JQuery} eleman 
+ * @param {number} gecikme 
+ */
 function efekUygula(eleman, gecikme) {
     eleman.fadeOut(gecikme / 2).fadeIn(gecikme);
     setTimeout(function() {
